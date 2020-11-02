200f74163cebee5aae3a076d3617fa5992e6b227
handhand <sohnjunior@naver.com>
2020-11-03 02:14:59 +0900

    [refactor] 웹팩 alias 설정
    + import 경로명 단축을 위해 alias 설정 적용

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@api': path.resolve(__dirname, 'src/api'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@views': path.resolve(__dirname, 'src/views'),
+        '@store': path.resolve(__dirname, 'src/store'),
+      },
+    },
+  },
+  transpileDependencies: ['vuetify'],
+};
