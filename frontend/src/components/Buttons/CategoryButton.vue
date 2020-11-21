f6975acf7211de7283fe92d3daf259d631004496
handhand <sohnjunior@naver.com>
2020-11-21 20:42:04 +0900

    [style] 카테고리 버튼 scss 적용 #59

--- frontend/src/components/Buttons/CategoryButton.vue
+++ frontend/src/components/Buttons/CategoryButton.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-btn color="light-green lighten-4" depressed x-large @click="onClick">
-    <v-img class="btn-image" :src="imageSrcMap[name]" />
-    <span class="ml-2 category"> {{ name }} </span>
-  </v-btn>
+  <button class="category-btn" @click="onClick">
+    <img class="category-btn__image" :src="imageSrcMap[name]" />
+    <span class="category-btn__text"> {{ name }} </span>
+  </button>
 </template>
 
 <script lang="ts">
@@ -11,11 +11,11 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 declare const require;
 
 const imageSrcMap = {
-  원룸: require('@/assets/room.svg'),
-  전공서적: require('@/assets/study.svg'),
-  회원권: require('@/assets/ticket.svg'),
-  의류: require('@/assets/clothes.svg'),
-  기타: require('@/assets/box.svg'),
+  원룸: require('@/assets/images/room.svg'),
+  전공서적: require('@/assets/images/study.svg'),
+  회원권: require('@/assets/images/ticket.svg'),
+  의류: require('@/assets/images/clothes.svg'),
+  기타: require('@/assets/images/box.svg'),
 };
 
 @Component
@@ -30,9 +30,27 @@ export default class CategoryButton extends Vue {
 }
 </script>
 
-<style scoped>
-.btn-image {
-  width: 30px;
-  height: 30px;
+<style lang="scss" scoped>
+$category-btn-color: #69db7c;
+
+.category-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 130px;
+  height: 50px;
+  border-radius: 10px;
+  background-color: $category-btn-color;
+
+  &__image {
+    width: 30px;
+    height: 30px;
+    margin-right: 6px;
+  }
+  &__text {
+    color: white;
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
 }
 </style>
