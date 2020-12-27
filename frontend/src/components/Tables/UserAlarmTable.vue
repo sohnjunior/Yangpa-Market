7e7a082380b269e89c8706ccc971ff6f58ca5dd8
handhand <sohnjunior@naver.com>
2020-12-27 12:18:47 +0900

    [feat] 테이블 api 데이터 파싱 프로시저 추가 #67

--- frontend/src/components/Tables/UserAlarmTable.vue
+++ frontend/src/components/Tables/UserAlarmTable.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <BaseTable :headers="orderHeaders" :items="orderList">
+    <BaseTable :headers="alarmHeaders" :items="orderAlarms">
       <template v-slot:table-name>
         <h1>구매요청 알림</h1>
       </template>
       <template v-slot:table-action="item">
-        <button @click="approve(item)">승인</button>
-        <button @click="remove(item)">삭제</button>
+        <button @click="onApprove(item)">승인</button>
+        <button @click="onRemove(item)">삭제</button>
       </template>
     </BaseTable>
   </div>
@@ -17,32 +17,48 @@ import { Component, Vue } from 'vue-property-decorator';
 import BaseTable from '@components/Tables/BaseTable.vue';
 import { OrderAPI } from '../../api';
 
-const orderHeaders = ['주문번호', '상품명', '연락처', '구매자', ''];
+interface IAlarmHistory {
+  id: number;
+  postId: number;
+  orderNumber: string;
+  productName: string;
+  contactNumber: string;
+  buyer: string;
+}
+
+function normalize(products: any[], order: any) {
+  return {
+    id: order.id,
+    postId: order.postId,
+    orderNumber: order.code,
+    productName: products[order.postId],
+    contactNumber: order.phone,
+    buyer: order.user.nickname,
+  };
+}
 
 @Component({
   components: { BaseTable },
 })
 export default class UserAlarmTable extends Vue {
-  private dialog: boolean = false;
-  private show: boolean = false;
-  private orderHeaders = orderHeaders;
-  private productList = {};
-  private orderList = [];
+  private alarmHeaders = ['주문번호', '상품명', '연락처', '구매자', ''];
+  private orderAlarms: IAlarmHistory[] = [];
 
-  public async created(): Promise<void> {
+  // FIXME: products 받아올 필요 없도록 API 수정
+  public async created() {
     const {
       data: { products, orders },
     } = await OrderAPI.fetchPendingOrder();
-    this.productList = products;
-    this.orderList = orders;
+
+    this.orderAlarms = orders.map((order) => normalize(products, order));
   }
 
-  public async approve(item): Promise<void> {
-    await OrderAPI.approveOrder({ postID: item.postId });
+  public onApprove(order: IAlarmHistory) {
+    OrderAPI.approveOrder({ postID: order.postId }); // FIXME: postId 대신 order.id 로 삭제하도록 API 변경
   }
 
-  async remove(item): Promise<void> {
-    await OrderAPI.rejectOrder(item.id);
+  public onRemove(order: IAlarmHistory) {
+    OrderAPI.rejectOrder(order.id);
   }
 }
 </script>
--- frontend/src/components/Tables/UserBuyingTable.vue
+++ frontend/src/components/Tables/UserBuyingTable.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <BaseTable :headers="orderPendingHeaders" :items="orderPendingList">
+    <BaseTable :headers="pendingTableHeaders" :items="pendingItems">
       <template v-slot:table-name>
         <h1>구매 대기</h1>
       </template>
     </BaseTable>
 
-    <BaseTable :headers="orderFinishedHeaders" :items="orderFinishedList">
+    <BaseTable :headers="purchasedTableHeaders" :items="purchasedItems">
       <template v-slot:table-name>
         <h1>구매 완료</h1>
       </template>
       <template v-slot:table-action>
-        <button @click="showReviewCreateModal">후기 등록</button>
+        <button @click="onShowModal">후기 등록</button>
       </template>
-      <ReviewCreateModal :show="show" @close-dialog="closeDialog()" />
+      <ReviewCreateModal :show="isOpenReviewModal" @close="onCloseModal" />
     </BaseTable>
   </div>
 </template>
@@ -24,41 +24,64 @@ import ReviewCreateModal from '@components/Modals/ReviewCreateModal.vue';
 import BaseTable from '@components/Tables/BaseTable.vue';
 import { OrderAPI } from '../../api';
 
-const orderPendingHeaders = ['상품명', '가격', '판매자', '판매자 연락처'];
+interface IPendingProductHistory {
+  productName: string;
+  price: number;
+  seller: string;
+  contactNumber: string;
+}
 
-const orderFinishedHeaders = ['상품명', '가격', '후기 작성'];
+interface IBoughtProductHistory {
+  productName: string;
+  price: number;
+}
+
+function normalizePendingProduct(info: any): IPendingProductHistory {
+  return {
+    productName: info.title,
+    price: info.price,
+    seller: info.user.nickname,
+    contactNumber: info.user.phone,
+  };
+}
+
+function normalizeBoughtProduct(info: any): IBoughtProductHistory {
+  return {
+    productName: info.title,
+    price: info.price,
+  };
+}
 
 @Component({
   components: { ReviewCreateModal, BaseTable },
 })
 export default class UserBuyingTable extends Vue {
-  private dialog: boolean = false;
-  private show: boolean = false;
-  private orderPendingList = [];
-  private orderFinishedList = [];
-  private orderPendingHeaders = orderPendingHeaders;
-  private orderFinishedHeaders = orderFinishedHeaders;
+  private isOpenReviewModal = false;
+  private pendingItems: IPendingProductHistory[] = [];
+  private purchasedItems: IBoughtProductHistory[] = [];
+  private pendingTableHeaders = ['상품명', '가격', '판매자', '판매자 연락처'];
+  private purchasedTableHeaders = ['상품명', '가격', ''];
 
   async created() {
     const {
       data: { infos },
     } = await OrderAPI.fetchPurchasedOrder();
 
-    this.orderPendingList = infos
-      .filter((orderInfo) => !orderInfo[1])
-      .map((orderInfo) => orderInfo[0]);
-    this.orderFinishedList = infos
-      .filter((orderInfo) => orderInfo[1])
-      .map((orderInfo) => orderInfo[0]);
+    for (const info of infos) {
+      const isOrdered = info[1];
+
+      if (isOrdered) this.purchasedItems.push(normalizeBoughtProduct(info[0]));
+      else this.pendingItems.push(normalizePendingProduct(info[0]));
+    }
   }
 
-  public showReviewCreateModal(): void {
+  public onShowModal() {
     // FIXME: 선택된 상품에 대한 리뷰를 남기도록 해야함
-    this.show = true;
+    this.isOpenReviewModal = true;
   }
 
-  public closeDialog(): void {
-    this.show = false;
+  public onCloseModal() {
+    this.isOpenReviewModal = false;
   }
 }
 </script>
