3169c9a4c1cc493b42310d18b7eec6e31581eddd
handhand <sohnjunior@naver.com>
2021-01-03 20:03:18 +0900

    [style] 기본 table 스타일 지정 #67

--- frontend/src/components/Tables/BaseTable.vue
+++ frontend/src/components/Tables/BaseTable.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <slot name="table-name"></slot>
-    <table>
+  <div class="table-container">
+    <slot name="table-name" />
+    <table class="table-wrapper">
       <thead>
-        <tr>
+        <tr class="table-header-row">
           <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in items" :key="index">
+        <tr class="table-content-row" v-for="(item, index) in items" :key="index">
           <td v-for="(cell, index) in item" :key="index">{{ cell }}</td>
-          <td><slot name="table-action" :item="item"></slot></td>
+          <td><slot name="table-action" :item="item" /></td>
         </tr>
       </tbody>
     </table>
@@ -27,4 +27,33 @@ export default class BaseTable extends Vue {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.table-container {
+  margin-bottom: 80px;
+
+  .table-wrapper {
+    width: 100%;
+    margin-top: 20px;
+
+    .table-header-row {
+      font-size: 0.8rem;
+      border-bottom: 1px solid #e4e7e8;
+
+      th {
+        padding: 15px 0px;
+      }
+    }
+
+    .table-content-row {
+      font-size: 1.1rem;
+      font-weight: 500;
+
+      td {
+        flex-basis: 10%;
+        padding: 15px 0px;
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
