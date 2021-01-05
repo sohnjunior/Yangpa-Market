d3b626d93c1c3a348aa47f40ddc04604db4b8258
handhand <sohnjunior@naver.com>
2021-01-05 21:56:52 +0900

    [feat] 댓글 입력 창 구현 #71

---
+++ frontend/src/components/Inputs/CommentInput.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="comment-input-container">
+    <div class="input-wrapper">
+      <textarea class="comment-input" type="text" placeholder="상품문의 입력" v-model="inputText" />
+    </div>
+    <div class="control-wrapper">
+      <label><input type="checkbox" name="secret" :checked="isSecret" />비밀댓글</label>
+      <button class="submit-btn" @click="onSubmitComment">등록</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { CommentAPI } from '../../api';
+
+@Component({})
+export default class CommentInput extends Vue {
+  @Prop({ required: true }) productID!: string;
+
+  private inputText = '';
+  private isSecret = false;
+
+  public async onSubmitComment() {
+    if (this.inputText === '') return;
+
+    try {
+      await CommentAPI.createComment({
+        postId: this.productID,
+        comment: this.inputText,
+        secret: this.isSecret,
+      });
+      this.$router.go(0);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/mixins';
+
+.comment-input-container {
+  display: flex;
+  flex-direction: column;
+  margin-left: 10%;
+  margin-right: 10%;
+  width: 80%;
+  border: 1px solid rgb(238, 238, 238);
+
+  .input-wrapper {
+    padding: 30px;
+
+    .comment-input {
+      width: 100%;
+      outline: none;
+      resize: none;
+    }
+  }
+
+  .control-wrapper {
+    display: flex;
+    padding: 15px;
+    border-top: inherit;
+
+    .submit-btn {
+      @include button();
+      margin-left: auto;
+      padding: 5px 25px;
+      background-color: #a5d8ff;
+      color: white;
+    }
+  }
+}
+</style>
