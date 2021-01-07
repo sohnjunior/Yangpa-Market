ba454aa0e021d1cd630fb2e45101fa7577e90ffc
handhand <sohnjunior@naver.com>
2021-01-07 19:46:41 +0900

    [feat] 메인뷰 하위 컴포넌트 분리 #73
    + 카테고리 리스트, 글 생성 버튼, 상품 carousel, 상품 그리드 리스트, 필터링 리스트

---
+++ frontend/src/components/Buttons/FloatingButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tooltip top>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        fab
+        dark
+        large
+        color="pink"
+        fixed
+        right
+        bottom
+        to="/product/new"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+    <span>새로운 상품을 등록해보세요!</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
