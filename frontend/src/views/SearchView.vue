06f098fd09675c9544bb5248aa88f5477e6cbc76
handhand <sohnjunior@naver.com>
2021-01-09 17:18:27 +0900

    [feat] 검색 뷰 중복 코드 제거 #80
    + 상품 내용, 판매자 정보가 없으므로 api 변경해서 추가하기

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,53 +1,42 @@
 <template>
-  <v-container>
-    <h1 class="mt-6 mb-4 sub-title">&quot;{{ keyword }}&quot; 에 대한 검색 결과</h1>
+  <div>
+    <h1>&quot;{{ keyword }}&quot; 에 대한 검색 결과</h1>
 
-    <v-container>
-      <v-row>
-        <ProductCard
-          v-for="(product, i) in products"
-          id="product-card"
-          :title="product.title"
-          :image="product.image"
-          :price="product.price"
-          :body="''"
-          :hit="product.post.hit"
-          :writer="''"
-          :like="product.like"
-          :productID="product.post.title"
-          :key="i"
-        />
-      </v-row>
-    </v-container>
-  </v-container>
+    <ProductGridList v-if="isProductsExist" :products="products" />
+    <div v-else>검색 결과에 해당하는 상품이 없어요</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { IPost } from '../types';
 import { ProductAPI } from '../api';
-import ProductCard from '@components/Cards/ProductCard.vue';
+import ProductGridList from '@components/Lists/ProductGridList.vue';
+import { IPost } from '../types';
 
+// FIXME: 현재 검색 결과로 넘어오는 데이터의 속성값이 IPost 인터페이스와 일치하지 않음(API 를 변경하는 방향으로 수정)
 @Component({
-  components: { ProductCard },
+  components: { ProductGridList },
 })
 export default class SearchView extends Vue {
-  private keyword: string = '';
+  private keyword = this.$route.params.keyword;
   private products: IPost[] = [];
 
+  get isProductsExist() {
+    return this.products.length !== 0;
+  }
+
   public async created() {
-    this.keyword = this.$route.params.keyword;
-    const {
-      data: { result },
-    } = await ProductAPI.searchProduct(this.keyword);
-    this.products = result;
+    try {
+      const {
+        data: { result },
+      } = await ProductAPI.searchProduct(this.keyword);
+
+      this.products = result;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 </script>
 
-<style scoped>
-#product-card {
-  margin-right: 20px;
-  margin-top: 20px;
-}
-</style>
+<style></style>
