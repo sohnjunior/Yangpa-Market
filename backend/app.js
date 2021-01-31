9ac95f9eb6267a4577287f7e68c9c2be42b4bf5b
handhand <sohnjunior@naver.com>
2021-01-31 22:54:36 +0900

    [chore] 주석 수정 #69
    + app.js, interceptor 주석 수정

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,12 @@ const app = express();
 sequelize.sync();
 passportConfig(passport);
 
-// view engine setup
+/** view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+/** cors */
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,17 +32,14 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// CORS 허용
-app.use(cors());
-
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
+/** catch 404 and forward to exception handler */
 app.use((req, res, next) => {
   next(new HTTP404Error('page not found'));
 });
 
-// error handler
+/** exception handler */
 app.use(handleErrors);
 
 module.exports = app;
