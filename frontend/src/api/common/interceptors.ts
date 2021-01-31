f26cbf062824ab507f82ada222576ff9944ec2c8
handhand <sohnjunior@naver.com>
2021-01-31 23:04:43 +0900

    [feat] 토큰 만료 시 리프레시 토큰 인증하도록 변경 #69

--- frontend/src/api/common/interceptors.ts
+++ frontend/src/api/common/interceptors.ts
@@ -1,10 +1,15 @@
 import store from '../../store';
+import AuthAPI from '../auth';
+
+function getBearerAccessToken() {
+  return `Bearer ${store.state.UserModule.accessToken}`;
+}
 
 export function setInterceptors(instance) {
   // Add a request interceptor
   instance.interceptors.request.use(
     function (config) {
-      config.headers.Authorization = `Bearer ${store.state.UserModule.accessToken}`;
+      config.headers.Authorization = getBearerAccessToken();
       return config;
     },
     function (error) {
@@ -17,7 +22,27 @@ export function setInterceptors(instance) {
     function (response) {
       return response;
     },
-    function (error) {
+    async function (error) {
+      const status = error.response.status;
+      const originalRequestConfig = error.config;
+
+      if (status === 419) {
+        /** 토큰 만료 시 */
+        try {
+          const refreshToken = store.state.UserModule.refreshToken;
+          const {
+            data: { accessToken },
+          } = await AuthAPI.fetchRefreshedAccessToken({ refreshToken });
+
+          store.commit('UserModule/setAccessToken', accessToken);
+          originalRequestConfig.headers.Authorization = getBearerAccessToken();
+          return instance.request(originalRequestConfig);
+        } catch (err) {
+          // TODO: 로그인 페이지로 리다이렉션
+          console.log('refresh 토큰 만료', err);
+        }
+      }
+
       return Promise.reject(error);
     }
   );
