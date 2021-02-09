91478a9ad3030a0c695f66c43416a890202122ff
handhand <sohnjunior@naver.com>
2021-02-10 01:53:14 +0900

    [feat] Error 클래스 타입 정의 #95

--- backend/utils/errors.ts
+++ backend/utils/errors.ts
@@ -6,44 +6,49 @@ const HTTP_STATUS_CODE = {
 };
 
 class BaseError extends Error {
-  constructor(name, httpCode, isOperational, message) {
+  name: string;
+  httpCode: number;
+  isOperational: boolean;
+
+  constructor(
+    name: string,
+    httpCode: number,
+    isOperational: boolean,
+    message: string
+  ) {
     super(message);
     this.name = name;
     this.httpCode = httpCode;
     this.isOperational = isOperational;
   }
 
-  getStatusCode = () => this.httpCode;
+  public getStatusCode() {
+    return this.httpCode;
+  }
 }
 
 class HTTP400Error extends BaseError {
-  constructor(message) {
+  constructor(message: string) {
     super('BAD_REQUEST', HTTP_STATUS_CODE.BAD_REQUEST, true, message);
   }
 }
 
 class HTTP401Error extends BaseError {
-  constructor(message) {
+  constructor(message: string) {
     super('UNAUTHORIZED', HTTP_STATUS_CODE.UNAUTHORIZED, true, message);
   }
 }
 
 class HTTP404Error extends BaseError {
-  constructor(message) {
+  constructor(message: string) {
     super('NOT_FOUND', HTTP_STATUS_CODE.NOT_FOUND, true, message);
   }
 }
 
 class HTTP419Error extends BaseError {
-  constructor(message) {
+  constructor(message: string) {
     super('TOKEN_EXPIRED', HTTP_STATUS_CODE.TOKEN_EXPIRED, true, message);
   }
 }
 
-module.exports = {
-  BaseError,
-  HTTP400Error,
-  HTTP401Error,
-  HTTP404Error,
-  HTTP419Error,
-};
+export { BaseError, HTTP400Error, HTTP401Error, HTTP404Error, HTTP419Error };
