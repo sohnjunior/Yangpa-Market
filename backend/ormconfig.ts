9299f573bd7f96ad72e7255a4e1430abb1c9fef7
handhand <sohnjunior@naver.com>
2021-02-11 02:10:09 +0900

    [BE] feat: typeORM 설정파일 추가 #95

---
+++ backend/ormconfig.ts
@@ -0,0 +1,11 @@
+export default {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
+  synchronize: true,
+  entities: ['models/*.ts'],
+};
