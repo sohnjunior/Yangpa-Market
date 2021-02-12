b5e135a5fc4158dd245914844f0309908d3d988b
handhand <sohnjunior@naver.com>
2021-02-12 23:27:30 +0900

    [BE] refactor: redis 클라이언트 클래스 TS 적용 #95

--- backend/redis/index.ts
+++ backend/redis/index.ts
@@ -1,19 +1,17 @@
-const redis = require('redis');
+import redis from 'redis';
 
 class RedisClient {
+  client: redis.RedisClient;
+
   constructor() {
     this.client = redis.createClient(6379);
   }
 
-  get endpoint() {
-    return this.client;
-  }
-
-  setValue(key, value) {
+  public setValue(key: string, value: string) {
     this.client.set(key, value);
   }
 
-  async getValue(key) {
+  public async getValue(key: string) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
         if (err) reject(err);
@@ -24,4 +22,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
