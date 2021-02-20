70b09a77592698a2ec0c4b2b0ce1b6864efb2790
handhand <sohnjunior@naver.com>
2021-02-20 22:07:23 +0900

    [FE] feat: 아바타 컴포넌트 추가 #101

---
+++ frontend/src/components/Common/Avatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="avatar" :src="src" alt="아바타 이미지" :width="width" :height="height" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class Avatar extends Vue {
+  @Prop({ default: 'http://localhost:3000/images/profile/default.png' }) src!: string;
+  @Prop({ default: 25 }) width!: number;
+  @Prop({ default: 25 }) height!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  border-radius: 50%;
+  object-fit: cover;
+}
+</style>
