d29ff1aec58431f1ead09c6aeea8e28f76efe3e2
handhand <sohnjunior@naver.com>
2021-04-23 00:02:34 +0900

    [FE] feat: 알림 테이블 제거 후 리스트 컴포넌트로 변경 #115
    + 리스트 스타일은 아직 미적용

---
+++ frontend/src/components/Lists/UserAlarmList.vue
@@ -0,0 +1,72 @@
+<template>
+  <ul class="alarm-container">
+    <li class="item-wrapper" v-for="item in alarmItems" :key="item.id">
+      <Icon filename="alarm" width="25" height="25" />
+      <div class="content-wrapper">
+        <Chip :text="item.type" width="55" />
+        <div>
+          <p class="message-wrapper" v-html="item.content" />
+          <div class="action-wrapper">
+            <button class="action-button approve" @click="onApprove(item.id)">승인</button>
+            <button class="action-button reject" @click="onReject(item.id)">거절</button>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import Icon from '@components/Common/Icon.vue';
+import Chip from '@components/Common/Chip.vue';
+
+interface IAlarmMessage {
+  id: number;
+  type: string;
+  content: string;
+}
+
+@Component({
+  components: { Icon, Chip },
+})
+export default class UserAlarmList extends Vue {
+  @Prop({ required: true }) readonly alarmItems!: IAlarmMessage[];
+
+  public onApprove(orderId: number) {
+    this.$emit('approve-order', orderId);
+  }
+
+  public onReject(orderId: number) {
+    this.$emit('reject-order', orderId);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// TODO: 스타일 적용하기
+.alarm-container {
+  .item-wrapper {
+    .content-wrapper {
+      .message-wrapper {
+        .message-body {
+        }
+
+        .message-footer {
+        }
+      }
+
+      .action-wrapper {
+        .action-button {
+        }
+
+        .approve {
+        }
+
+        .reject {
+        }
+      }
+    }
+  }
+}
+</style>
